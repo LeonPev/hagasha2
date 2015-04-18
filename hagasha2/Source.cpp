994d1db46c4358f5f60283eac025f14599d27f61
Leon Pevzner <leonpevzner@gmail.com>
2015-04-18 22:55:19 +0300

    a

--- hagasha2/Source.cpp
+++ hagasha2/Source.cpp
@@ -10,8 +10,7 @@ int main()
 	int i, j, nArrIndex, mArrIndex;
 	int arrN[N] = { 1, 2, 3, 4, 5, 6, 7, 8 };
 	int arrM[M] = { 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 };
-	nArrIndex =  N / 2;
-
+	nArrIndex =  N / 2;//************************
 	mArrIndex = M / 2;
 
 
