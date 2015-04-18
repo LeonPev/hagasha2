f69508f45617f2ae2a7c307410c80ea7de22c3f7
osher <osherztikz@gmail.com>
2015-04-18 22:20:46 +0300

    changed for

--- hagasha2/Source.cpp
+++ hagasha2/Source.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 
 #define N 8
-#define M 8
+#define M 16
 
 int main()
 {
-	int i, j, nArrIndex, mArrIndex;
+	int i,j, nArrIndex, mArrIndex;
 	int arrN[N] = { 1, 2, 3, 4, 5, 6, 7, 8 };
-	int arrM[M] = { 9, 10, 11, 12, 13, 14, 15, 16 };
+	int arrM[M] = { 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 };
 	nArrIndex = mArrIndex = N / 2;
+
 
 	for (i = N / 2, j = M / 2; i > 1; i /= 2, j /= 2)
 	{
@@ -31,11 +32,11 @@ int main()
 	}
 	if (arrN[nArrIndex] < arrM[mArrIndex])
 	{
-		cout << "index: " << mArrIndex - 1 << "result : " << arrM[mArrIndex - 1] << endl;
+		cout << "index: " << mArrIndex - 1 << " result : " << arrM[mArrIndex - 1] << endl;
 	}
 	else
 	{
-		cout << "index: " << nArrIndex - 1 << "result : " << arrN[nArrIndex - 1] << endl;
+		cout << "index: " << nArrIndex - 1 << " result : " << arrN[nArrIndex - 1] << endl;
 	}
 	cin >> i;
 	return 0;
